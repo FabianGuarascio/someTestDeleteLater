04d1f54e1e923c5c67ebf464f992cc418742da87
FabianGuarascio <guarascio.fabiand@gmail.com>
2025-02-24 20:08:30 -0300

    feat: generates some better types for the project

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,4 +1,3 @@
-// longitude: number, latitude: number, boundingBox: any, width: number, height: number
 export type Coordinates = {
   longitude: number, 
   latitude: number
@@ -17,3 +16,64 @@ export type BoundingBox = {
 
 export type ProjectCreation = Coordinates & WidthAndHeight & {boundingBox:BoundingBox}
 
+type Geometry<T extends 'Polygon' | 'MultiPolygon' | 'LineString'> = {
+  type: T;
+  coordinates: T extends 'Polygon'
+    ? number[][][] 
+    : T extends 'MultiPolygon'
+    ? number[][][][] 
+    : T extends 'LineString'
+    ? number[][] 
+    : never;
+};
+
+export type GeoJSONFeature<T extends 'Polygon' | 'MultiPolygon' | 'LineString' = 'Polygon' | 'MultiPolygon' | 'LineString'> = {
+  type: 'Feature';
+  geometry: Geometry<T>;
+  properties: {
+    name?: string;
+    [key: string]: any; 
+  };
+};
+
+
+export type GeoJSONData = {
+  type: 'FeatureCollection';
+  features: GeoJSONFeature[];
+}
+
+
+export type USAPoint= {
+  longitude: number;
+  latitude: number;
+  count: number;
+}
+
+
+export type USALineFeature ={
+  type: 'Feature';
+  geometry: {
+    type: 'LineString';
+    coordinates: number[][];
+  };
+  properties: {
+    [key: string]: any; 
+  };
+}
+
+export type USALinesData = {
+  type: 'FeatureCollection';
+  features: USALineFeature[];
+}
+
+export function isPolygon(geometry: Geometry<any>): geometry is Geometry<'Polygon'> {
+  return geometry.type === 'Polygon';
+}
+
+export function isMultiPolygon(geometry: Geometry<any>): geometry is Geometry<'MultiPolygon'> {
+  return geometry.type === 'MultiPolygon';
+}
+
+export function isLineString(geometry: Geometry<any>): geometry is Geometry<'LineString'> {
+  return geometry.type === 'LineString';
+}
